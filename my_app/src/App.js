968607d3e01723f7810a852e5c6f1581daa1fcde
RomanMelnyk909 <pvrrivne@gmail.com>
2023-11-14 21:04:18 +0200

    added registration form with inputs

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -3,11 +3,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
 
-import { useState } from 'react';
-
-
 const App = () => {
-
   return (
     <div className="App">
       <Header />
