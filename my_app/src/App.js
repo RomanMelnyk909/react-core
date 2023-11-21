5ff00f46c9c762a6224d34d00ee99643379ca5f5
RomanMelnyk909 <45914004+RomanMelnyk909@users.noreply.github.com>
2023-11-21 16:44:54 +0200

    added context and router

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -4,17 +4,24 @@ import Footer from "./components/Footer";
 import Main from "./components/Main";
 import { Routes, Route } from "react-router-dom";
 import Products from "./components/Products";
+import { createContext, useState } from "react";
+
+export const Context = createContext();
 
 const App = () => {
+  const [usersCount, setUsersCount] = useState(0);
   return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/products" element={<Products />} />
-      </Routes>
-      <Footer />
-    </div>
+    <Context.Provider value={{ usersCount, setUsersCount }}>
+      <div className="App">
+        {usersCount}
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Context.Provider>
   );
 };
 
--- my_app/src/components/RegistrationForm/RegistrationForm.jsx
+++ my_app/src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,52 +1,58 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Input from "../Input";
 import UserCard from "../UserCard/UserCard";
 
-import { v4 as getUniqueId } from 'uuid';
+import { v4 as getUniqueId } from "uuid";
+import { Context } from "../../App";
 
 import styles from "./registrationForm.module.css";
 
 const DEFAULT_USERS = [
   {
-    name: 'John',
-    surname: 'Nollan',
-    email: 'john@gmail.com',
-    id: getUniqueId()
+    name: "John",
+    surname: "Nollan",
+    email: "john@gmail.com",
+    id: getUniqueId(),
   },
   {
-    name: 'Nick',
-    surname: 'Rozberg',
-    email: 'nick',
-    id: getUniqueId()
+    name: "Nick",
+    surname: "Rozberg",
+    email: "nick",
+    id: getUniqueId(),
   },
   {
-    name: 'Anna',
-    surname: 'Lee',
-    email: 'anna@gmail.com',
-    id: getUniqueId()
+    name: "Anna",
+    surname: "Lee",
+    email: "anna@gmail.com",
+    id: getUniqueId(),
   },
   {
-    name: 'Rafael',
-    surname: 'Rafff',
-    email: 'rafael@gmail.com',
-    id: getUniqueId()
+    name: "Rafael",
+    surname: "Rafff",
+    email: "rafael@gmail.com",
+    id: getUniqueId(),
   },
 ];
 
 const RegistrationForm = () => {
+  const { usersCount, setUsersCount } = useContext(Context);
+
+  console.log(usersCount);
+
   const [users, setUsers] = useState(DEFAULT_USERS);
-  const [isEditMode, setIsEditMode] = useState(true);
+  const [isEditMode, setIsEditMode] = useState(false);
 
   const [name, setName] = useState();
   const [surname, setSurname] = useState();
   const [email, setEmail] = useState();
+  // const [currentId, setCurrentId] = useState();
 
   const onAddUser = () => {
     const user = {
       name,
       surname,
       email,
-      id: getUniqueId()
+      id: getUniqueId(),
     };
 
     setUsers([...users, user]);
@@ -54,7 +60,6 @@ const RegistrationForm = () => {
     setSurname("");
     setEmail("");
   };
-
 
   const onGetName = (value) => {
     setName(value);
@@ -70,21 +75,38 @@ const RegistrationForm = () => {
 
   const onDeleteUserHandler = (id) => {
     const filteredUsers = users.filter((user) => user.id !== id);
-      setUsers(filteredUsers)  
+    setUsers(filteredUsers);
   };
 
   const onUpdateUserHandler = (id) => {
     const currentUser = users.filter((user) => user.id === id)[0];
-  
+
     setName(currentUser.name);
     setSurname(currentUser.surname);
     setEmail(currentUser.email);
+    // setCurrentId(currentUser.id);
+    // setIsEditMode(true);
   };
 
+  // const onSaveUser = () => {
+  //   setUsers(
+  //     users.map((user) => {
+  //       if (user.id === currentId) {
+  //         return { ...user, name, surname, email };
+  //       }
+  //       return user;
+  //     })
+  //   );
+  //   setName("");
+  //   setSurname("");
+  //   setEmail("");
+  //   setCurrentId(null);
+  //   setIsEditMode(false);
+  // };
 
   return (
-    <div className={styles['common']}>
-      <div className={styles['left-side']}>
+    <div className={styles["common"]}>
+      <div className={styles["left-side"]}>
         <Input
           label="Name: "
           placeholder="Enter Your Name"
@@ -103,20 +125,26 @@ const RegistrationForm = () => {
           onChangeFunction={onGetEmail}
           value={email}
         />
-        {
-          isEditMode ? (
-            <button type="button" onClick={onAddUser} className={styles['add-user-button']}>
-              Update User
-            </button>
-          ) : (
-            <button type="button" onClick={onAddUser} className={styles['add-user-button']}>
-               Add User
-            </button>
-          )
-        }
+        {isEditMode ? (
+          <button
+            type="button"
+            // onClick={onSaveUser}
+            className={styles["add-user-button"]}
+          >
+            Save User Data
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={onAddUser}
+            className={styles["add-user-button"]}
+          >
+            Add User
+          </button>
+        )}
       </div>
-      <div className={styles['right-side']}>
-        <div className={styles['users-list']}>
+      <div className={styles["right-side"]}>
+        <div className={styles["users-list"]}>
           {users.map((user, index) => {
             const { name, surname, email, id } = user;
             return (
